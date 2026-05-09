5469576df58e8a84c858d27458c6dc315a6f3683
dgarciaCl <danig.ocana@gmail.com>
2026-05-09 20:18:44 +0200

    Implementation of evaluation metrics

--- Metrics.py
+++ Metrics.py
@@ -3,32 +3,50 @@ import numpy as np
 
 def MAE(y_true: np.ndarray, y_pred: np.ndarray) -> np.float64:
     """ Implementation of the Mean Absolute Error (MAE) """
-    ### TO BE COMPLETED BY THE STUDENTS ###
-    
-    ### DUMMY BEHAVIOUR TO PREVENT CRASHING (MUST BE DELETED AFTER THE FULL IMPLEMENTATION) ###
-    return 0.0
-    ### ###
+    sum = 0
+
+    for i in range(len(y_true)):
+        sum += abs(y_true[i] - y_pred[i])
+
+    return sum / len(y_true)
 
 def MSE(y_true: np.ndarray, y_pred: np.ndarray) -> np.float64:
     """ Implementation of the Mean Squared Error (MSE) """
-    ### TO BE COMPLETED BY THE STUDENTS ###
-    
-    ### DUMMY BEHAVIOUR TO PREVENT CRASHING (MUST BE DELETED AFTER THE FULL IMPLEMENTATION) ###
-    return 0.0
-    ### ###
+    sum = 0
+
+    for i in range(len(y_true)):
+        sum += (y_true[i] - y_pred[i]) ** 2
+
+    return sum / len(y_true)
 
 def R2(y_true: np.ndarray, y_pred: np.ndarray) -> np.float64:
     """ Implementation of the R2 metric """
-    ### TO BE COMPLETED BY THE STUDENTS ###
-    
-    ### DUMMY BEHAVIOUR TO PREVENT CRASHING (MUST BE DELETED AFTER THE FULL IMPLEMENTATION) ###
-    return 0.0
-    ### ###
+    mean_true = 0
+
+    for i in range(len(y_true)):
+        mean_true += y_true[i] / len(y_true) 
+
+    regressor, mean_based_estimator = 0
+
+    for i in range(len(y_true)):
+        regressor += (y_true[i] - y_pred[i]) ** 2
+        mean_based_estimator += (y_true[i] - mean_true) ** 2
+
+    return 1 - (regressor / mean_based_estimator)
 
 def Corr(y_true: np.ndarray, y_pred: np.ndarray) -> np.float64:
     """ Implementation of the Pearson's Correlation Coefficient """
-    ### TO BE COMPLETED BY THE STUDENTS ###
-    
-    ### DUMMY BEHAVIOUR TO PREVENT CRASHING (MUST BE DELETED AFTER THE FULL IMPLEMENTATION) ###
-    return 0.0
-    ### ###
+    mean_true, mean_pred = 0
+
+    for i in range(len(y_true)):
+        mean_true += y_true[i] / len(y_true)
+        mean_pred += y_pred[i] / len(y_true)
+
+    variance_true, variance_pred, covariance_true_pred = 0
+
+    for i in range(len(y_true)):
+        variance_true += (y_true[i] - mean_true) ** 2 / len(y_true)
+        variance_pred += (y_pred[i] - mean_pred) ** 2 / len(y_true)
+        covariance_true_pred += (y_true[i] - mean_true) * (y_pred[i] - mean_pred) / len(y_true)
+
+    return covariance_true_pred / np.sqrt(variance_true * variance_pred)
